08a89fa1be3605424e0eb49c347bdc0f90581206
ListKelvin <phamm5687@gmai.com>
2022-09-06 20:56:34 +0700

    handle add to cart , create cart context helper func

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import "./cart-dropdown.styles.scss";
 import React from "react";
 import "../button/button.component";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>GO TO CHECkOUT</Button>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
       </div>
+      <Button>GO TO CHECkOUT</Button>
     </div>
   );
 };
